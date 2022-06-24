4adba6487f19cb31a598f4e95c41c00c24c0a5f5
Ana Cecilia Souza <anaceciams@gmail.com>
2022-06-24 08:54:46 -0300

    criação da classe ContaCorrente

---
+++ MeuPrimeiroPrograma/ContaCorrente.cs
@@ -0,0 +1,7 @@
+public class ContaCorrente // acessada de qualquer lugar do código 
+{
+    public string Titular;
+    public int Agencia;
+    public int Numero;
+    public double Saldo;
+}
--- MeuPrimeiroPrograma/Program.cs
+++ MeuPrimeiroPrograma/Program.cs
@@ -9,9 +9,8 @@ namespace meuPrimeiroProjeto
            string Titular = "Ana";
            int Agencia = 123;
            int Conta = 456;
-           double Saldo = 100;  
-           
-                    
+           double Saldo = 100; 
+           Console.WriteLine("Titular: " +Titular+ " Conta: " +Conta+ " Agencia: " +Agencia+ " Saldo: " +Saldo);
         }
     }
 } 
