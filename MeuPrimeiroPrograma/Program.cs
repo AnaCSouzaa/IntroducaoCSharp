3e6ebb200424bfc47e7986ed0cb163d63f90b6cc
Ana Cecilia Souza <anaceciams@gmail.com>
2022-06-24 08:11:50 -0300

    exemplo com uso de conta bancária

--- MeuPrimeiroPrograma/Program.cs
+++ MeuPrimeiroPrograma/Program.cs
@@ -6,10 +6,12 @@ namespace meuPrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá, mundo!");
-            Console.WriteLine("Esse é o nosso primeiro programa C#!");
-            Console.WriteLine("Aula dia 21/06!");
+           string Titular = "Ana";
+           int Agencia = 123;
+           int Conta = 456;
+           double Saldo = 100;  
            
+                    
         }
     }
 } 
