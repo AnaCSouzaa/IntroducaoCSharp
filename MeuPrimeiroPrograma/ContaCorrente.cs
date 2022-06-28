7d2f1a08413afa4e881bc520eef27956b0b40666
Ana Cecilia Souza <anaceciams@gmail.com>
2022-06-28 16:56:36 -0300

    criação de contas através de um construtor

--- MeuPrimeiroPrograma/ContaCorrente.cs
+++ MeuPrimeiroPrograma/ContaCorrente.cs
@@ -1,7 +1,18 @@
 public class ContaCorrente // acessada de qualquer lugar do código 
 {
-    public string Titular;
+    public string Titular; //atributos da classe (características)
     public int Agencia;
     public int Numero;
     public double Saldo;
-}
+
+    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo) 
+    //construtor: possui as informações necessárias para a criação de um objeto dentro de uma classe. 
+    //Nesse caso, precisa haver, necessariamente, o titular, agência, número e saldo para criação da conta corrente. 
+    {
+        this.Titular = Titular; //o segundo Titular é considerado variável
+        this.Agencia = Agencia;
+        this.Numero = Numero;
+        this.Saldo = Saldo;
+    }
+
+} 
--- MeuPrimeiroPrograma/Program.cs
+++ MeuPrimeiroPrograma/Program.cs
@@ -6,11 +6,15 @@ namespace meuPrimeiroProjeto
     {
         static void Main(string[] args)
         {
-           string Titular = "Ana";
-           int Agencia = 123;
-           int Conta = 456;
-           double Saldo = 100; 
-           Console.WriteLine("Titular: " +Titular+ " Conta: " +Conta+ " Agencia: " +Agencia+ " Saldo: " +Saldo);
+           ContaCorrente contaAna = new ContaCorrente ("Ana", 123, 456, 1000); //passa a existir um novo objeto do tipo conta corrente 
+           Console.WriteLine("A conta é do(a): " +contaAna.Titular+ ", a agência é: " +contaAna.Agencia+ " e o número é: " +contaAna.Numero+ "."); 
+           
+           ContaCorrente contaBernardo = new ContaCorrente ("Bernardo", 123, 321, 190);  
+           Console.WriteLine("A conta é do(a): " +contaBernardo.Titular+ ", a agência é: " +contaBernardo.Agencia+ " e o número é: " +contaBernardo.Numero+ "."); 
+
+           ContaCorrente contaCarlos = new ContaCorrente ("Carlos", 123, 562, 500);  
+           Console.WriteLine("A conta é do(a): " +contaCarlos.Titular+ ", a agência é: " +contaCarlos.Agencia+ " e o número é: " +contaCarlos.Numero+ "."); 
+
         }
     }
 } 
