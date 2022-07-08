2fbacd5953240de4b42028612cdf933abb378198
Ana Cecilia Souza <anaceciams@gmail.com>
2022-07-08 09:02:33 -0300

    Criação do método Depositar

--- MeuPrimeiroPrograma/ContaCorrente.cs
+++ MeuPrimeiroPrograma/ContaCorrente.cs
@@ -1,4 +1,6 @@
-public class ContaCorrente // public = acessada de qualquer lugar do código 
+public class ContaCorrente 
+// instanciar = criar objetos dentro de uma determinada classe, torná-la concreta
+// public = acessada de qualquer lugar do código 
 {
     public string Titular; //atributos da classe (características)
     public int Agencia;
@@ -24,6 +26,19 @@ public class ContaCorrente // public = acessada de qualquer lugar do código
         if (this.Saldo >= valorSaque)
         {
             this.Saldo = this.Saldo - valorSaque;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Depositar(double valorDeposito)
+    //método = sempre será verbo
+    // entre o tipo do método,'public', e o método, 'Sacar', há a informação do tipo de retorno.
+    // só há método quando houver um objeto atrelado a ele, por isso pode-se utilizar o 'this' para referenciar algum objeto 
+    {
+        if (valorDeposito > 0)
+        {
+            this.Saldo = this.Saldo + valorDeposito;
             return true;
         }
         return false;
--- MeuPrimeiroPrograma/Program.cs
+++ MeuPrimeiroPrograma/Program.cs
@@ -10,16 +10,26 @@ namespace meuPrimeiroProjeto
            Console.WriteLine("A conta é do(a): " +contaAna.Titular+ ", a agência é: " +contaAna.Agencia+ ", o número é: " +contaAna.Numero+ " e o saldo é: " +contaAna.Saldo+ "."); 
            bool sacarContaAna = contaAna.Sacar(500); //o tipo da variável tem que ser do mesmo que o retorno atribuído ao método
            Console.WriteLine("A conta é do(a): " +contaAna.Titular+ ", a agência é: " +contaAna.Agencia+ ", o número é: " +contaAna.Numero+ " e o saldo é: " +contaAna.Saldo+ "."); 
+           bool depositarContaAna = contaAna.Depositar(300);
+           Console.WriteLine("A conta é do(a): " +contaAna.Titular+ ", a agência é: " +contaAna.Agencia+ ", o número é: " +contaAna.Numero+ " e o saldo é: " +contaAna.Saldo+ "."); 
+           Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------"); 
+
 
            ContaCorrente contaBernardo = new ContaCorrente ("Bernardo", 123, 321, 190);  
            Console.WriteLine("A conta é do(a): " +contaBernardo.Titular+ ", a agência é: " +contaBernardo.Agencia+ ", o número é: " +contaBernardo.Numero+ " e o saldo é: " +contaBernardo.Saldo+ "."); 
            bool sacarContaBernardo = contaBernardo.Sacar(50);
            Console.WriteLine("A conta é do(a): " +contaBernardo.Titular+ ", a agência é: " +contaBernardo.Agencia+ ", o número é: " +contaBernardo.Numero+ " e o saldo é: " +contaBernardo.Saldo+ "."); 
+           bool depositarContaBernardo = contaBernardo.Depositar(100);
+           Console.WriteLine("A conta é do(a): " +contaBernardo.Titular+ ", a agência é: " +contaBernardo.Agencia+ ", o número é: " +contaBernardo.Numero+ " e o saldo é: " +contaBernardo.Saldo+ "."); 
+           Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------"); 
 
            ContaCorrente contaCarlos = new ContaCorrente ("Carlos", 123, 562, 500);  
            Console.WriteLine("A conta é do(a): " +contaCarlos.Titular+ ", a agência é: " +contaCarlos.Agencia+ ", o número é: " +contaCarlos.Numero+ " e o saldo é: " +contaCarlos.Saldo+ "."); 
            bool sacarContaCarlos = contaCarlos.Sacar(500);
            Console.WriteLine("A conta é do(a): " +contaCarlos.Titular+ ", a agência é: " +contaCarlos.Agencia+ ", o número é: " +contaCarlos.Numero+ " e o saldo é: " +contaCarlos.Saldo+ "."); 
+           bool depositarContaCarlos = contaCarlos.Depositar(90);
+           Console.WriteLine("A conta é do(a): " +contaCarlos.Titular+ ", a agência é: " +contaCarlos.Agencia+ ", o número é: " +contaCarlos.Numero+ " e o saldo é: " +contaCarlos.Saldo+ "."); 
+           Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------"); 
 
         }
     }
